b377ea36d9da2a4f1c95385b58a703d53df99675
Vathana <Vathana@gmail.com>
2026-04-07 11:24:47 +0700

    Update API

--- src/Components/API/api.jsx
+++ src/Components/API/api.jsx
@@ -18,7 +18,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "https://laravel-test-api-qpy0.onrender.com"
 });
 // https://laravel-test-api-qpy0.onrender.com/api/student
 // http://127.0.0.1:8000
